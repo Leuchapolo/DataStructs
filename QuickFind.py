2044bd75685d2f7cc289068139b5b606e9ab8f9d
Michael Bellissimo <leucha@Michaels-MacBook-Pro.local>
2019-02-28 13:33:16 -0500

    Refactored

--- QuickFind.py
+++ QuickFind.py
@@ -4,27 +4,17 @@ class QuickFind:
 		self.parents = []
 		for i in range(n):
 			self.parents.append(i)
+
 	def check(self, a, b):
-		if(a >= self.n or b >= self.n):
-			print("Input out of range")
-			return
-		rootA,rootB = (-1,-1)
-		currentA, currentB = (a, b)
-		while (rootA == -1 or rootB == -1):
-			if( currentA == self.parents[currentA]):
-				rootA = currentA
-			else: 
-				currentA = self.parents[currentA]
-			if(currentB == self.parents[currentB]):
-				rootB = currentB
-			else: 
-				currentB = self.parents[currentB]
-		if(rootA == rootB):
-			return True
-		else:
-			return False
+		return root(a) == root(b)
+
+	def root(self, a):
+		while a != self.parents[a]:
+			a = self.parents[a]
+
+
 	def union(self, a, b):
-		self.parents[b] = a
+		self.parents[root(b)] = root(a)
 
 
 
